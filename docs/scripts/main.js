c761cd46008376d019b9a58b813e726415c2da9b
hero2bash <zero.bash@gmail.com>
2020-02-06 15:58:38 +0800

    Initial Commit

---
+++ docs/scripts/main.js
@@ -0,0 +1,21 @@
+import view from './view.js';
+
+/**
+ * Yes we're using document functions in the main.js but it's
+ * one of the rare cases where it's allowed. Almost everything else
+ * should be in the view.js
+ */
+function assignEventListeners(){
+    document.getElementById('gen-orange')
+    .addEventListener('click', function(){
+        view.changeToOrange();
+    })
+
+    document.getElementById('gen-violet')
+    .addEventListener('click', function(){
+        view.changeToViolet();
+    })
+}
+
+
+assignEventListeners();
---
+++ docs/scripts/view.js
@@ -0,0 +1,9 @@
+export default {
+    changeToOrange(){
+        document.body.style.backgroundColor = '#FF4F1F';
+    },
+
+    changeToViolet(){
+        document.body.style.backgroundColor = '#5D3E5D';
+    }
+}
